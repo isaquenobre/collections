a758ef56fd845e7493f4f94bbe6b23ff74ffaa8b
isaque <isaquenobrec@gmail.com>
2025-02-18 17:32:08 -0300

    segundo commit

---
+++ src/collection2023_25/Collection2023_25.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package collection2023_25;
+
+/**
+ *
+ * @author Windows 10 PRO
+ */
+public class Collection2023_25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
