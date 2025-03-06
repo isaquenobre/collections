dc8d4763a956ecdb769037ce5cc924849969e7a8
isaque <isaquenobrec@gmail.com>
2025-03-05 23:25:40 -0300

    "agora o Set"

---
+++ src/set_inteface/operacoes_basicas_set/ConjuntoConvidados.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package set_inteface.operacoes_basicas_set;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Windows 10 PRO
+ */
+public class ConjuntoConvidados {
+    
+    private Set<Convidado> convidadoSet;
+    
+}
