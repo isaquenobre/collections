74b8450aa4c90700b4546d4e56176a4f785ba1c7
isaque <isaquenobrec@gmail.com>
2025-03-22 13:47:09 -0300

    "agora o Set----"

--- src/set_inteface/operacoes_basicas_set/ConjuntoConvidados.java
+++ src/set_inteface/operacoes_basicas_set/ConjuntoConvidados.java
@@ -42,4 +42,22 @@ public class ConjuntoConvidados {
     public void exibirConvidados(){
         System.out.println(convidadoSet);
     }
-}
+    
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set convidados");
+        
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+        
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set convidados");
+        
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set convidados");
+        
+        conjuntoConvidados.exibirConvidados();
+    }
+} 
