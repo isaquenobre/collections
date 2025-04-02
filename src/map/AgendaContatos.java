f4f9d4e19e3cbc0f167a48a319338a5171912e8d
isaque <isaquenobrec@gmail.com>
2025-04-01 23:37:35 -0300

    HashMap - contato

--- src/map/AgendaContatos.java
+++ src/map/AgendaContatos.java
@@ -44,6 +44,26 @@ public class AgendaContatos {
         
         return numeroPorNome;
         }
+    
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+        
+        agendaContatos.exibirContatos();
+        
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 5665);
+        agendaContatos.adicionarContato("Camila Cavalcante", 1111111);
+        agendaContatos.adicionarContato("Camila DIO", 654897);
+        agendaContatos.adicionarContato("Maria Silva", 1111111);
+        agendaContatos.adicionarContato("Camila", 44444);
+        
+        agendaContatos.exibirContatos();
+        
+        agendaContatos.removerContato("Maria Silva");
+        agendaContatos.exibirContatos();
+        
+        System.out.println("O numero é " + agendaContatos.pesquisarPorNome("Camila DIO"));
+    }
     }
     
     
