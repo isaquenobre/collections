d0a5b720c8db133eee21981a261a5cc3ee4526dd
isaque <isaquenobrec@gmail.com>
2025-04-08 15:10:31 -0300

    map

--- src/map/EstoqueProdutos.java
+++ src/map/EstoqueProdutos.java
@@ -55,5 +55,19 @@ public class EstoqueProdutos {
         return produtoMaisCaro;
     }
     
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        
+        estoque.exibirProduto();
+        
+        estoque.adicionarProduto(1L, "Produto A", 10, 5.0);
+        estoque.adicionarProduto(2L, "Produto B", 5, 10.0);
+        estoque.adicionarProduto(3L, "Prduto C", 2, 15.0);
+        
+        estoque.exibirProduto();
+        
+        System.out.println("Valor total do estoque: R$:" + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais barato:" + estoque.obterProdutoMaisCaro());
+    }
     
 }
